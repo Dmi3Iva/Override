97db8554cdcf8168719ac872aa15d8474d002561
Dddd <dddd@ddd.com>
2023-02-28 01:23:03 +0300

    добавил уровень 02, а так же перевод 00 и новый код для 01

---
+++ level02/Resources/script.sh
@@ -0,0 +1,5 @@
+echo 756e505234376848 | xxd -r -p | rev;
+echo 45414a3561733951 | xxd -r -p | rev;
+echo 377a7143574e6758 | xxd -r -p | rev; 
+echo 354a35686e475873 | xxd -r -p | rev; 
+echo 48336750664b394d | xxd -r -p | rev;
